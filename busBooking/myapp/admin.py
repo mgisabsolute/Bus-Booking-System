977562693e4d831d88b5dbd1f2fd7e19019260e8
Manan Gupta <manangupta9997@gmail.com>
2025-02-18 10:40:11 +0530

    Update admin.py

--- busBooking/myapp/admin.py
+++ busBooking/myapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Bus, User, Book
+from .models import Wallet
 
 # Register your models here.
+admin.site.register(Bus)
+admin.site.register(User)
+admin.site.register(Book)
+admin.site.register(Wallet)
